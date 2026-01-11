0ced5ba5855f6f4cf27b2fd39df8a2713ee2db81
Olivia He <oliviahe@169-231-192-170.wireless.ucsb.edu>
2026-01-10 16:24:39 -0800

    store user input when submit

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const taskForm = document.getElementById("taskInfo");
 addTaskBtn.addEventListener("click", function() {
     taskForm.style.display = "block";
 });
+
+const current = document.getElementById("currentTasks");
+
+taskForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const task = {
+        className: document.getElementById("className").value,
+        assignment: document.getElementById("taskName").value,
+        dueDate: document.getElementById("dueDate")
+    };
+
+    current.textContent += "<p>task</p>"
+
+    taskForm.reset();
+    taskForm.style.display = "none"
+});
